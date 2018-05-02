dd2bba8b7ba2febc6c1cc4b6b52246edfbd002b4
Xue Chen <xueqingc@gmail.com>
2018-05-01 21:35:36 -0700

    refactor code to ES6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee{
   // TODO..
-};
+  constructor() {
+    super()
+    this.age = 40
+    this.job = 'gamble'
+    this.canFly = false
+    this.color = 'grey'
+    this.treasureChest = []
+  }
+  forage() {
+    super.forage()
+    return 'I am too old, let me play cards instead'
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure)
+  }
+}
